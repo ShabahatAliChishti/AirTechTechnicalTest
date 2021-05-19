40c21b1725f76da7f14617f29f886555a57b265a
shabahat97 <shabahatalichishti@gmail.com>
2021-05-19 16:25:28 +0500

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AirTechTechnicalTest
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Controllers/DetailSearchController.cs
@@ -0,0 +1,33 @@
+﻿using AirTechTechnicalTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+namespace AirTechTechnicalTest.Controllers
+{
+    public class DetailSearchController : Controller
+    {
+        AirTechTechnicalTestEntities db = new AirTechTechnicalTestEntities();
+        // GET: DetailSearch
+        public ActionResult Index()
+        {
+            List<Location> CountryList = db.Locations.ToList();
+            ViewBag.CountryList = new SelectList(CountryList, "LocationId", "LocationName");
+            return View();
+        }
+
+
+        //[HttpPost]
+        public ActionResult FilterByPrice(HomeSearching model,int minPrice,int maxPrice,double maxarea,double minarea)
+        {
+           
+            ViewBag.filterByPrice = true;
+            var filterHome = db.HomeSearchings.Where(x => x.Price >= minPrice && x.Price <= maxPrice&& x.Area >= minarea && x.Area <=maxarea && x.LocationId==model.LocationId&&x.Office==model.Office&&x.Bed>=model.Bed&&x.Baths>=model.Baths).ToList();
+            //return View("Products", filterProducts.ToPagedList(page ?? 1, 9));
+            return PartialView(@"~/Views/Shared/_Home.cshtml", filterHome);
+
+        }
+    }
+}
